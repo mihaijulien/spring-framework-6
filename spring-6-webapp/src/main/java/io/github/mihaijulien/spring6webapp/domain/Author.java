d75daf32600164a0677f627f496d369e0c73368c
Julien Mihai <mihaijulien@gmail.com>
2024-09-26 10:24:30 +0300

    Hibernate equality: equals(), hashCode() and toString()

--- spring-6-webapp/src/main/java/io/github/mihaijulien/spring6webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/io/github/mihaijulien/spring6webapp/domain/Author.java
@@ -13,6 +13,8 @@ public class Author {
     private String firstName;
     private String lastName;
 
+    // An author can have multiple books and a book can have multiple authors (Many to Many)
+    // It is better to use Set than List because each Book should be a unique item and a List allows duplicate items
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
@@ -46,5 +48,31 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    // It is important to create the equals() and hashCode() methods because Hibernate uses them internally to determine
+    // equality between objects.
+    // There are a couple of strategies that can be implemented, such as using only the Id field or all fields of the entity.
+    // In this example, only the Id field will be used
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+
+        return getId() != null ? getId().equals(author.getId()) : author.getId() == null;
+    }
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- spring-6-webapp/src/main/java/io/github/mihaijulien/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/io/github/mihaijulien/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package io.github.mihaijulien.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -13,6 +14,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    // A book can have several authors and an author can have several books (Many to Many)
+    // In the Join table we indicate that we have a table called author_book. One part of the join column,
+    // the one referring to this Entity, is book_id and the reverse join, the one referring to the Author entity, is author_id
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -50,4 +54,32 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    // It is important to create the equals() and hashCode() methods because Hibernate uses them internally to determine
+    // equality between objects.
+    // There are a couple of strategies that can be implemented, such as using only the Id field or all fields of the entity.
+    // In this example, only the Id field will be used
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+
+        return getId() != null ? getId().equals(book.getId()) : book.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+
 }
